ff94da619adbf911efb49fbc79385382c1c3da8d
nidak0658 <165899145+nidak0658@users.noreply.github.com>
2024-04-03 22:33:59 +0500

    Add files via upload

---
+++ project11/dist/main.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log('hello world 12345');
+console.log('hello world 11');
+console.log('hello world 45');
+console.log('hello world 11 45 8');
+console.log('hello world 00000');
---
+++ project11/src/main.ts
@@ -0,0 +1,5 @@
+console.log('hello world 12345')
+console.log('hello world 11')
+console.log('hello world 45')
+console.log('hello world 11 45 8')
+console.log('hello world 00000')
